225d99a80f4bb6aa50ac18398f1b435d1942dcce
Carl Andrei Lambino <veriasdan@gmail.com>
2022-07-01 16:23:47 +0800

    searchform design complete

--- CTA/CTA/SearchForm.Designer.cs
+++ CTA/CTA/SearchForm.Designer.cs
@@ -28,12 +28,684 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.LBL2 = new System.Windows.Forms.Label();
+            this.MonthBox = new System.Windows.Forms.ComboBox();
+            this.DayBox = new System.Windows.Forms.ComboBox();
+            this.YearBox = new System.Windows.Forms.ComboBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.ext = new System.Windows.Forms.Button();
+            this.SearchWindow = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl1.Location = new System.Drawing.Point(12, 18);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(332, 33);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "When did you filled up?";
+            // 
+            // LBL2
+            // 
+            this.LBL2.AutoSize = true;
+            this.LBL2.BackColor = System.Drawing.Color.Transparent;
+            this.LBL2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.LBL2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LBL2.Location = new System.Drawing.Point(20, 63);
+            this.LBL2.Name = "LBL2";
+            this.LBL2.Size = new System.Drawing.Size(46, 19);
+            this.LBL2.TabIndex = 0;
+            this.LBL2.Text = "Date";
+            // 
+            // MonthBox
+            // 
+            this.MonthBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MonthBox.FormattingEnabled = true;
+            this.MonthBox.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.MonthBox.Location = new System.Drawing.Point(20, 85);
+            this.MonthBox.Name = "MonthBox";
+            this.MonthBox.Size = new System.Drawing.Size(93, 22);
+            this.MonthBox.TabIndex = 1;
+            this.MonthBox.Text = "Month";
+            // 
+            // DayBox
+            // 
+            this.DayBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DayBox.FormattingEnabled = true;
+            this.DayBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.DayBox.Location = new System.Drawing.Point(119, 85);
+            this.DayBox.Name = "DayBox";
+            this.DayBox.Size = new System.Drawing.Size(46, 22);
+            this.DayBox.TabIndex = 2;
+            this.DayBox.Text = "Day";
+            // 
+            // YearBox
+            // 
+            this.YearBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.YearBox.FormattingEnabled = true;
+            this.YearBox.Items.AddRange(new object[] {
+            "2000",
+            "2001",
+            "2002",
+            "2003",
+            "2004",
+            "2005",
+            "2006",
+            "2007",
+            "2008",
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+            "2022",
+            "2023",
+            "2024",
+            "2025",
+            "2026",
+            "2027",
+            "2028",
+            "2029",
+            "2030",
+            "2031",
+            "2032",
+            "2033",
+            "2034",
+            "2035",
+            "2036",
+            "2037",
+            "2038",
+            "2039",
+            "2040",
+            "2041",
+            "2042",
+            "2043",
+            "2044",
+            "2045",
+            "2046",
+            "2047",
+            "2048",
+            "2049",
+            "2050",
+            "2051",
+            "2052",
+            "2053",
+            "2054",
+            "2055",
+            "2056",
+            "2057",
+            "2058",
+            "2059",
+            "2060",
+            "2061",
+            "2062",
+            "2063",
+            "2064",
+            "2065",
+            "2066",
+            "2067",
+            "2068",
+            "2069",
+            "2070",
+            "2071",
+            "2072",
+            "2073",
+            "2074",
+            "2075",
+            "2076",
+            "2077",
+            "2078",
+            "2079",
+            "2080",
+            "2081",
+            "2082",
+            "2083",
+            "2084",
+            "2085",
+            "2086",
+            "2087",
+            "2088",
+            "2089",
+            "2090",
+            "2091",
+            "2092",
+            "2093",
+            "2094",
+            "2095",
+            "2096",
+            "2097",
+            "2098",
+            "2099",
+            "2100",
+            "2101",
+            "2102",
+            "2103",
+            "2104",
+            "2105",
+            "2106",
+            "2107",
+            "2108",
+            "2109",
+            "2110",
+            "2111",
+            "2112",
+            "2113",
+            "2114",
+            "2115",
+            "2116",
+            "2117",
+            "2118",
+            "2119",
+            "2120",
+            "2121",
+            "2122",
+            "2123",
+            "2124",
+            "2125",
+            "2126",
+            "2127",
+            "2128",
+            "2129",
+            "2130",
+            "2131",
+            "2132",
+            "2133",
+            "2134",
+            "2135",
+            "2136",
+            "2137",
+            "2138",
+            "2139",
+            "2140",
+            "2141",
+            "2142",
+            "2143",
+            "2144",
+            "2145",
+            "2146",
+            "2147",
+            "2148",
+            "2149",
+            "2150",
+            "2151",
+            "2152",
+            "2153",
+            "2154",
+            "2155",
+            "2156",
+            "2157",
+            "2158",
+            "2159",
+            "2160",
+            "2161",
+            "2162",
+            "2163",
+            "2164",
+            "2165",
+            "2166",
+            "2167",
+            "2168",
+            "2169",
+            "2170",
+            "2171",
+            "2172",
+            "2173",
+            "2174",
+            "2175",
+            "2176",
+            "2177",
+            "2178",
+            "2179",
+            "2180",
+            "2181",
+            "2182",
+            "2183",
+            "2184",
+            "2185",
+            "2186",
+            "2187",
+            "2188",
+            "2189",
+            "2190",
+            "2191",
+            "2192",
+            "2193",
+            "2194",
+            "2195",
+            "2196",
+            "2197",
+            "2198",
+            "2199",
+            "2200",
+            "2201",
+            "2202",
+            "2203",
+            "2204",
+            "2205",
+            "2206",
+            "2207",
+            "2208",
+            "2209",
+            "2210",
+            "2211",
+            "2212",
+            "2213",
+            "2214",
+            "2215",
+            "2216",
+            "2217",
+            "2218",
+            "2219",
+            "2220",
+            "2221",
+            "2222",
+            "2223",
+            "2224",
+            "2225",
+            "2226",
+            "2227",
+            "2228",
+            "2229",
+            "2230",
+            "2231",
+            "2232",
+            "2233",
+            "2234",
+            "2235",
+            "2236",
+            "2237",
+            "2238",
+            "2239",
+            "2240",
+            "2241",
+            "2242",
+            "2243",
+            "2244",
+            "2245",
+            "2246",
+            "2247",
+            "2248",
+            "2249",
+            "2250",
+            "2251",
+            "2252",
+            "2253",
+            "2254",
+            "2255",
+            "2256",
+            "2257",
+            "2258",
+            "2259",
+            "2260",
+            "2261",
+            "2262",
+            "2263",
+            "2264",
+            "2265",
+            "2266",
+            "2267",
+            "2268",
+            "2269",
+            "2270",
+            "2271",
+            "2272",
+            "2273",
+            "2274",
+            "2275",
+            "2276",
+            "2277",
+            "2278",
+            "2279",
+            "2280",
+            "2281",
+            "2282",
+            "2283",
+            "2284",
+            "2285",
+            "2286",
+            "2287",
+            "2288",
+            "2289",
+            "2290",
+            "2291",
+            "2292",
+            "2293",
+            "2294",
+            "2295",
+            "2296",
+            "2297",
+            "2298",
+            "2299",
+            "2300",
+            "2301",
+            "2302",
+            "2303",
+            "2304",
+            "2305",
+            "2306",
+            "2307",
+            "2308",
+            "2309",
+            "2310",
+            "2311",
+            "2312",
+            "2313",
+            "2314",
+            "2315",
+            "2316",
+            "2317",
+            "2318",
+            "2319",
+            "2320",
+            "2321",
+            "2322",
+            "2323",
+            "2324",
+            "2325",
+            "2326",
+            "2327",
+            "2328",
+            "2329",
+            "2330",
+            "2331",
+            "2332",
+            "2333",
+            "2334",
+            "2335",
+            "2336",
+            "2337",
+            "2338",
+            "2339",
+            "2340",
+            "2341",
+            "2342",
+            "2343",
+            "2344",
+            "2345",
+            "2346",
+            "2347",
+            "2348",
+            "2349",
+            "2350",
+            "2351",
+            "2352",
+            "2353",
+            "2354",
+            "2355",
+            "2356",
+            "2357",
+            "2358",
+            "2359",
+            "2360",
+            "2361",
+            "2362",
+            "2363",
+            "2364",
+            "2365",
+            "2366",
+            "2367",
+            "2368",
+            "2369",
+            "2370",
+            "2371",
+            "2372",
+            "2373",
+            "2374",
+            "2375",
+            "2376",
+            "2377",
+            "2378",
+            "2379",
+            "2380",
+            "2381",
+            "2382",
+            "2383",
+            "2384",
+            "2385",
+            "2386",
+            "2387",
+            "2388",
+            "2389",
+            "2390",
+            "2391",
+            "2392",
+            "2393",
+            "2394",
+            "2395",
+            "2396",
+            "2397",
+            "2398",
+            "2399",
+            "2400",
+            "2401",
+            "2402",
+            "2403",
+            "2404",
+            "2405",
+            "2406",
+            "2407",
+            "2408",
+            "2409",
+            "2410",
+            "2411",
+            "2412",
+            "2413",
+            "2414",
+            "2415",
+            "2416",
+            "2417",
+            "2418",
+            "2419",
+            "2420",
+            "2421",
+            "2422",
+            "2423",
+            "2424",
+            "2425",
+            "2426",
+            "2427",
+            "2428",
+            "2429",
+            "2430",
+            "2431",
+            "2432",
+            "2433",
+            "2434",
+            "2435",
+            "2436",
+            "2437",
+            "2438",
+            "2439",
+            "2440",
+            "2441",
+            "2442",
+            "2443",
+            "2444",
+            "2445",
+            "2446",
+            "2447",
+            "2448",
+            "2449",
+            "2450",
+            "2451",
+            "2452",
+            "2453",
+            "2454",
+            "2455",
+            "2456",
+            "2457",
+            "2458",
+            "2459",
+            "2460",
+            "2461",
+            "2462",
+            "2463",
+            "2464",
+            "2465",
+            "2466",
+            "2467",
+            "2468",
+            "2469",
+            "2470",
+            "2471",
+            "2472",
+            "2473",
+            "2474",
+            "2475",
+            "2476",
+            "2477",
+            "2478",
+            "2479",
+            "2480",
+            "2481",
+            "2482",
+            "2483",
+            "2484",
+            "2485",
+            "2486",
+            "2487",
+            "2488",
+            "2489",
+            "2490",
+            "2491",
+            "2492",
+            "2493",
+            "2494",
+            "2495",
+            "2496",
+            "2497",
+            "2498",
+            "2499",
+            "2500"});
+            this.YearBox.Location = new System.Drawing.Point(171, 85);
+            this.YearBox.Name = "YearBox";
+            this.YearBox.Size = new System.Drawing.Size(55, 22);
+            this.YearBox.TabIndex = 3;
+            this.YearBox.Text = "Year";
+            // 
+            // Search
+            // 
+            this.Search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Search.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Search.Location = new System.Drawing.Point(17, 118);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 4;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = false;
+            // 
+            // ext
+            // 
+            this.ext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ext.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ext.Location = new System.Drawing.Point(255, 418);
+            this.ext.Name = "ext";
+            this.ext.Size = new System.Drawing.Size(75, 23);
+            this.ext.TabIndex = 5;
+            this.ext.Text = "Exit";
+            this.ext.UseVisualStyleBackColor = false;
+            // 
+            // SearchWindow
+            // 
+            this.SearchWindow.Location = new System.Drawing.Point(17, 148);
+            this.SearchWindow.Name = "SearchWindow";
+            this.SearchWindow.Size = new System.Drawing.Size(311, 260);
+            this.SearchWindow.TabIndex = 0;
+            this.SearchWindow.Text = "";
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SearchForm";
+            this.BackgroundImage = global::CTA.Properties.Resources.bgimg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(352, 454);
+            this.Controls.Add(this.SearchWindow);
+            this.Controls.Add(this.ext);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.YearBox);
+            this.Controls.Add(this.DayBox);
+            this.Controls.Add(this.MonthBox);
+            this.Controls.Add(this.LBL2);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SearchForm";
+            this.Text = "Checking for your record?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label LBL2;
+        private System.Windows.Forms.ComboBox MonthBox;
+        private System.Windows.Forms.ComboBox DayBox;
+        private System.Windows.Forms.ComboBox YearBox;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button ext;
+        private System.Windows.Forms.RichTextBox SearchWindow;
     }
 }
